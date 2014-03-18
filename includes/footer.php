e32c01ee82d533f68532e9a9b23cc1b2cbbe268e
Fran Levy <flevy@post.harvard.edu>
2014-03-18 01:01:54 -0500

    Tried to add/fix Github Font Icons for the social media section of the footer. It's not displaying correctly, and in researching a solution, I've come across arguments against using font icons at all--such as screen-reader problems and browser font customization issues. So I'll probably ditch the font icons entirely in favor of SVG images, if I can get them.

--- includes/footer.php
+++ includes/footer.php
@@ -1,16 +1,26 @@
     <!-- Footer -->
     <footer>
+      <!-- Social Media Links -->
       <div class='social-media'>
         <div class='container'>
           <ul>
-            <li data-icon="\f00a">Github</li>
-            <li data-icon="\f03b">Email</li>
+            <li>
+              <a href="https://github.com/franlevy/franlevy">
+                <span class=​"mega-octicon octicon-mark-github" title=​"GitHub">​
+                </span>
+              </a>
+            </li>
+            <li>
+              <a href="contact.php"
+                <span class=​"mega-octicon octicon-mail" title=​"Email">​
+                </span>
+              </a>
+            </li>
           </ul>
-          
-        
         </div>
       </div>
       <div class='site-copy-wrap'>
+        <!-- Site Map -->
         <div class='site-map'>
           <div class='container'>
             <ul>
@@ -26,6 +36,7 @@
             </ul>
           </div>
         </div>
+        <!-- Copyright -->
         <div class='copyright'>
           <p>
             Copyright &copy; <?php echo date('Y'); ?> Fran Levy. All rights reserved.
