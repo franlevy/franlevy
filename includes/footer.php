89153dfa51ec1dff632c6a3e443a68899eba5587
Fran Levy <flevy@post.harvard.edu>
2014-03-18 15:47:31 -0500

    Tested my website for accessibility. I was missing html defining my website's language as english, so I added it (U.S. English, specifically) as a wrap around the head and body html.

--- includes/footer.php
+++ includes/footer.php
@@ -1,48 +1,49 @@
-    <!-- Footer -->
-    <footer>
-      <!-- Social Media Links -->
-      <div class='social-media'>
-        <div class='container'>
-          <ul>
-            <li>
-              <a href="https://github.com/franlevy/franlevy">
-                <span class=​"mega-octicon octicon-mark-github" title=​"GitHub">​
-                </span>
-              </a>
-            </li>
-            <li>
-              <a href="contact.php"
-                <span class=​"mega-octicon octicon-mail" title=​"Email">​
-                </span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class='site-copy-wrap'>
-        <!-- Site Map -->
-        <div class='site-map'>
+      <!-- Footer -->
+      <footer>
+        <!-- Social Media Links -->
+        <div class='social-media'>
           <div class='container'>
             <ul>
               <li>
-                <a href='#'>Portfolio</a>
+                <a href="https://github.com/franlevy/franlevy">
+                  <span class=​"mega-octicon octicon-mark-github" title=​"GitHub">​
+                  </span>
+                </a>
               </li>
               <li>
-                <a href='#'>About</a>
-              </li>
-              <li>
-                <a href='#'>Contact</a>
+                <a href="contact.php"
+                  <span class=​"mega-octicon octicon-mail" title=​"Email">​
+                  </span>
+                </a>
               </li>
             </ul>
           </div>
         </div>
-        <!-- Copyright -->
-        <div class='copyright'>
-          <p>
-            Copyright &copy; <?php echo date('Y'); ?> Fran Levy. All rights reserved.
-          </p>
+        <div class='site-copy-wrap'>
+          <!-- Site Map -->
+          <div class='site-map'>
+            <div class='container'>
+              <ul>
+                <li>
+                  <a href='#'>Portfolio</a>
+                </li>
+                <li>
+                  <a href='#'>About</a>
+                </li>
+                <li>
+                  <a href='#'>Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <!-- Copyright -->
+          <div class='copyright'>
+            <p>
+              Copyright &copy; <?php echo date('Y'); ?> Fran Levy. All rights reserved.
+            </p>
+          </div>
         </div>
-      </div>
-    </footer>
-  </body>
+      </footer>
+    </body>
+  </html>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -1,38 +1,39 @@
 <!DOCTYPE html>
 <html xmlns='http://www.w3.org/1999.html'>
-  <head>    
-    <title><?php echo $pageTitle; ?> | Fran Levy</title>
-    <meta content='width=device-width, initial-scale=1' name='viewport'>
-    <link href='stylesheets/global.css' rel='stylesheet' type='text/css'>
-    <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js'></script>
-    <script src='javascript/script.js'></script>
-  </head>
-  <body>
-    <!-- Header and Navigation -->
-    <header>
-      <div class='container'>
-        <div class='title'>
-          <h1>
-            <a href='index.html'>Fran Levy</a>
-          </h1>
+  <html lang="en-US">  
+    <head>    
+      <title><?php echo $pageTitle; ?> | Fran Levy</title>
+      <meta content='width=device-width, initial-scale=1' name='viewport'>
+      <link href='stylesheets/global.css' rel='stylesheet' type='text/css'>
+      <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js'></script>
+      <script src='javascript/script.js'></script>
+    </head>
+    <body>
+      <!-- Header and Navigation -->
+      <header>
+        <div class='container'>
+          <div class='title'>
+            <h1>
+              <a href='index.html'>Fran Levy</a>
+            </h1>
+          </div>
+          <div class='nav-btn'>
+            <a class='navicon link' href='#mainmenu'></a>
+          </div>
+          <div id='mainmenu'>
+            <nav>
+              <ul>
+                <li>
+                  <a href='#'>Portfolio</a>
+                </li>
+                <li>
+                  <a href='#'>About</a>
+                </li>
+                <li>
+                  <a href='#'>Contact</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
-        <div class='nav-btn'>
-          <a class='navicon link' href='#mainmenu'></a>
-        </div>
-        <div id='mainmenu'>
-          <nav>
-            <ul>
-              <li>
-                <a href='#'>Portfolio</a>
-              </li>
-              <li>
-                <a href='#'>About</a>
-              </li>
-              <li>
-                <a href='#'>Contact</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </header>
+      </header>
