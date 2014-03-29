04d9719d5c9099a83336c955852bf6a8ca62c19d
Fran Levy <flevy@post.harvard.edu>
2014-03-29 00:59:51 -0500

    Saved various portfolio images in SVG file format for future grumpicons. Added the website images to the Portfolio page. Need to add the graphic design images next (once they're saved as SVG's), and format the Portfolio page appropriately.

--- portfolio.php
+++ portfolio.php
@@ -11,13 +11,22 @@ include("includes/header.php");
     <article>
       <h3></h3>
       <div class='category'>
+        <img src='images/websites/force-web-proposal_p1.svg'>
+      </div>
+      <div class='category'>
         <img src='images/websites/LizKalish_screenshot.png'>
       </div>
       <div class='category'>
         <img src='images/websites/WIWYK_screenshot.png'>
       </div>
       <div class='category'>
-        <p></p>
+        <img src='images/websites/cck-web-proposal_p1.svg'>
+      </div>
+      <div class='category'>
+        <img src='images/websites/bpi-web-proposal_p1.svg'>
+      </div>
+      <div class='category'>
+        <img src='images/graphic-design/gsd-V3-book-spread.svg'>
       </div>
     </article>
     <article>
